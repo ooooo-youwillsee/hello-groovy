4761502b9089d6116e1136b3325b58c82c72d977
ooooo <297872913@qq.com>
2022-12-02 10:39:34 +0800

    chapter3

---
+++ src/main/groovy/com/ooooo/chapter3/TakeHelp.groovy
@@ -0,0 +1,50 @@
+package com.ooooo.chapter3
+
+/**
+ * @author <a href="https://github.com/ooooo-youwillsee">ooooo</a>
+ * @since 1.0.0
+ */
+
+def takeHelp(helper) {
+  helper.helpMoveThings()
+}
+
+
+class Man {
+  void helpMoveThings() {
+    println "Man's helping"
+  }
+}
+
+class Woman {
+  void helpMoveThings() {
+    println "Woman's helping"
+  }
+}
+
+class Elephant {
+  void helpMoveThings() {
+    println "Elephant's helping"
+  }
+
+  void eatSugarcane() {
+    println "I love sugarcanes"
+  }
+}
+
+
+takeHelp(new Man())
+takeHelp(new Woman())
+takeHelp(new Elephant())
+
+def takeHelpAndReward(helper) {
+  helper.helpMoveThings()
+
+  if (helper.metaClass.respondsTo(helper, "eatSugarcane")) {
+    helper.eatSugarcane()
+  }
+}
+
+takeHelpAndReward(new Man())
+takeHelpAndReward(new Woman())
+takeHelpAndReward(new Elephant())
